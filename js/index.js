128c07c58a2a9c3149220e62ba73c86018be5e2e
unknown <jesdagonpas@gmail.com>
2018-06-03 22:09:24 +0100

    Reservas acabado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var windowActual = $('#home');
     var marcadoActual = $('.home > font');
+    var estadoBoton = "rp";
 
     $('.destinos').click(function () {
         windowActual.removeClass();
@@ -33,6 +34,44 @@ $(document).ready(function () {
         $('.excursiones > font').addClass('marcado');
         marcadoActual = $('.excursiones > font');
         windowActual = $('#excursiones');
+    });
+
+    $('.reservas').click(function () {
+        windowActual.removeClass();
+        windowActual.addClass('oculto');
+        marcadoActual.removeClass('marcado');
+        $('#reservas').removeClass();
+        $('#reservas').addClass('carousel slide paramenos');
+        $('.reservas > font').addClass('marcado');
+        marcadoActual = $('.reservas > font');
+        windowActual = $('#reservas');
+    });
+
+    $("#type").change(function () {
+        var val = $(this).val();
+        if (val == "d_gc") {
+            $("#size").html("<option value='ac'>Avistamiento de cetáceos</option><option value='gcda'>Gran Canaria desde el aire</option><option value='dpgcda'>Descubre el pequeño gran continente desde el aire</option><option value='sbv'>Senderismo en Bandama y vinos</option>");
+        } else if (val == "d_t") {
+            $("#size").html("<option value='ct'>Las cañadas del Teide</option>");
+        } else if (val == "d_f") {
+            $("#size").html("<option value='ail'>Aventura en Isla de Lobos</option>");
+        } else if (val == "d_l") {
+            $("#size").html("<option value='gtl'>Gran Tour de Lanzarote</option>");
+        } else if (val == "d_g") {
+            $("#size").html("<option value='vi'>Vuelta a la isla</option>");
+        } else if (val == "d_p") {
+            $("#size").html("<option value='gtl'>Senderismo en el Bosque Encantado</option>");
+        } else if (val == "d_h") {
+            $("#size").html("<option value='tih'>Tour isla de El Hierro</option>");
+        }
+    });
+
+    $(".pago").click(function () {
+            $("#para-reservar").addClass('oculto');
+            $("#para-pagar").removeClass('oculto');
+            $(this).text('PAGAR');
+            estadoBoton = "pr";
+            $("#reservas").addClass('paramas');
     });
 
     var $videoSrc;
